34cd19d853c8664fb062690598d84df639eae786
mahsin <mahsin1912@gmail.com>
2023-10-18 21:58:02 +0600

    Mongodb database connected

--- index.js
+++ index.js
@@ -24,10 +24,13 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const brandCollection = client.db("brandDB").collection("brands");
 
-
-
-
+    app.get("/brands", async(req, res) => {
+      const result = await brandCollection.find().toArray();
+      console.log(result)
+      res.send(result);
+    })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
