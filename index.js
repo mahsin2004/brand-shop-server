c7827487d5be2cab2e0e0656eddac7c7384f6b36
mahsin <mahsin1912@gmail.com>
2023-10-21 13:26:20 +0600

    fixed Delete route methode related issue

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ async function run() {
 
     app.delete("/myCart/:id", async(req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
+      const query = { _id: id};
       const result = await myCartCollection.deleteOne(query);
       res.send(result);
     });
